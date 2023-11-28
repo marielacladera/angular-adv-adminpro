fcb0de4c6c450f51cca6e2ffb0487ceeba82b598
marielaCladera <marielacladera@gmail.com>
2023-11-28 16:17:26 -0400

    estructuracion del proyecto y diseno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphic1Component } from './pages/graphic1/graphic1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NotpagefoundComponent,
+    DashboardComponent,
+    BreadcrumsComponent,
+    HeaderComponent,
+    AsideComponent,
+    ProgressComponent,
+    Graphic1Component,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagefound',
+  templateUrl: './notpagefound.component.html',
+  styleUrl: `./notpagefound.component.css`
+})
+export class NotpagefoundComponent {
+  
+  private year: number;
+
+  constructor( 
+    
+  ) {
+    this.year = new Date().getFullYear(); 
+  }
+
+  public getYear() {
+    return this.year;
+  }
+}
